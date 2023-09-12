27efc823133b93236ba317c7016c66db00904025
Chaeyeon1475 <chaney2225@gmail.com>
2023-09-12 22:26:48 +0900

    Initial commit

---
+++ src/main/java/com/hanium/seeku/RestApiTest.java
@@ -0,0 +1,15 @@
+package com.hanium.seeku;
+import io.fabric8.kubernetes.api.model.*;
+import io.fabric8.kubernetes.api.model.Pod;
+import io.fabric8.kubernetes.api.model.PodList;
+import io.fabric8.kubernetes.client.Config;
+import io.fabric8.kubernetes.client.ConfigBuilder;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.List;
+
+public class RestApiTest
+{
+}
---
+++ src/test/java/com/hanium/seeku/SeekuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanium.seeku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeekuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
